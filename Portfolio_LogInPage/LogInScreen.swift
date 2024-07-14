f9aa316ec663bd08fb46bfd658982ce5b45689cd
Vince Muller <vince.muller@redeye.co>
2024-07-14 13:45:30 -0600

    Initial Commit

---
+++ Portfolio_LogInPage/LogInScreen.swift
@@ -0,0 +1,72 @@
+//
+//  LogInScreen.swift
+//  Portfolio_LogInPage
+//
+//  Created by Vince Muller on 7/12/24.
+//
+
+import SwiftUI
+
+struct LogInScreen: View {
+    @State private var username: String = ""
+    @State private var password: String = ""
+    
+    var body: some View {
+        ZStack {
+            Color(.offWhite)
+                .ignoresSafeArea(.all)
+            ZStack {
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(.white)
+                    .frame(width: 360, height: 300)
+                    .offset(y: 20)
+                VStack {
+                    Text("Vince Muller")
+                        .font(.system(size: 25, weight: .semibold, design: .serif))
+                    Text("iOS Development Portfolio")
+                }.offset(y: -250)
+                VStack (spacing: 5) {
+                        HStack (alignment: .bottom) {
+                            Image(systemName: "person.fill")
+                                .font(.system(size: 20))
+                            TextField("Username", text: $username)
+                                .font(.system(size: 20))
+                                .offset(y: 4)
+                        }.frame(width: 300)
+                        Rectangle()
+                        .frame(width: 300, height: 1)
+                    HStack (alignment: .bottom) {
+                        Image(systemName: "lock.fill")
+                            .font(.system(size: 20))
+                        TextField("Password", text: $password)
+                            .font(.system(size: 20))
+                            .offset(y: 3)
+                    }
+                    .frame(width: 300)
+                    .padding(.top, 25)
+                    Rectangle()
+                    .frame(width: 300, height: 1)
+                    Button(action: {print("executed!")}, label: {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 15)
+                                .fill(.black)
+                                .frame(width: 300, height: 50)
+                            Text("Log In")
+                                .foregroundStyle(.white)
+                                .font(.title2)
+                        }
+                    }).offset(y: 50)
+                    Button(action: {print("Password Reset!")}, label: {
+                        Text("Forgot Password?")
+                            .font(.system(size: 14))
+                            .foregroundStyle(.black)
+                    }).offset(y: 60)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    LogInScreen()
+}
---
+++ Portfolio_LogInPage/Portfolio_LogInPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Portfolio_LogInPageApp.swift
+//  Portfolio_LogInPage
+//
+//  Created by Vince Muller on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Portfolio_LogInPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LogInScreen()
+        }
+    }
+}
